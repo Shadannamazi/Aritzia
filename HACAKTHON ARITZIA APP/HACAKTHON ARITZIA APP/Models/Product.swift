ceb7efa9bfc2dd6f49a2d04e7bbc03fbddb624a2
lava05 <lava05@students.cs.ubc.ca>
2023-01-13 23:44:32 -0800

    renamed bottom and added names to products

--- HACAKTHON ARITZIA APP/HACAKTHON ARITZIA APP/Models/Product.swift
+++ HACAKTHON ARITZIA APP/HACAKTHON ARITZIA APP/Models/Product.swift
@@ -15,14 +15,14 @@ struct Product: Identifiable {
     var type: String
 }
 
-var productList = [Product(name: "Pink Top", color: "pink", image: "t1", type: "top"),
-                   Product(name: "White Top", color: "white", image: "t2", type: "top"),
-                   Product(name: "Black Puffer", color: "black", image: "t3", type: "top"),
-                   Product(name: "Black Top", color: "black", image: "t4", type: "top"),
-                   Product(name: "Blue Top", color: "blue", image: "t5", type: "top"),
-                   Product(name: "Skirt", color: "beige", image: "b1", type: "bottem"),
-                   Product(name: "Leather Pants", color: "brown", image: "b2", type: "bottem"),
-                   Product(name: "Dress Pant", color: "brown", image: "b3", type: "bottem"),
-                   Product(name: "Cargo", color: "blue", image: "b4", type: "bottem"),
-                   Product(name: "Sweatpants", color: "red", image: "b5", type: "bottem")
+var productList = [Product(name: "Lydia Top", color: "pink", image: "t1", type: "top"),
+                   Product(name: "Sculpt Knit Criss Cross Tank", color: "white", image: "t2", type: "top"),
+                   Product(name: "The Super Puff Shorty", color: "black", image: "t3", type: "top"),
+                   Product(name: "Contour Scoopneck Longsleeve", color: "black", image: "t4", type: "top"),
+                   Product(name: "Sculpt Knit Criss Cross Cropped Tank", color: "blue", image: "t5", type: "top"),
+                   Product(name: "Sunday Best Rider Skirt", color: "beige", image: "b1", type: "bottom"),
+                   Product(name: "The Melina Pant", color: "brown", image: "b2", type: "bottom"),
+                   Product(name: "Dashwood Pant", color: "brown", image: "b3", type: "bottom"),
+                   Product(name: "Supply Cargo Pant", color: "blue", image: "b4", type: "bottom"),
+                   Product(name: "Cozy Fleece Mega Sweatpant", color: "red", image: "b5", type: "bottom")
 ]
